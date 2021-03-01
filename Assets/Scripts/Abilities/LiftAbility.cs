7d8601113fc8a29a256bd3190a53c526729db3d2
Thomasjnliu <thomasjnliu@gmail.com>
2021-03-01 00:02:36 -0500

    Make box collider a trigger when being lifted

--- Assets/Scripts/Abilities/LiftAbility.cs
+++ Assets/Scripts/Abilities/LiftAbility.cs
@@ -19,6 +19,10 @@ public class LiftAbility : MonoBehaviour
                 // player.playerSpeed = 5f;
                 player.box.transform.SetParent(player.transform, true);
                 player.box.GetComponent<Rigidbody>().isKinematic = true;
+
+                // was having some buggy behaviour when the player's collider intersected with the box's collider
+                // by setting the box collider to a trigger, it makes it so other colliders don't interact with it in physics
+                player.box.GetComponent<Collider>().isTrigger = true;
                 PlayerSoundController.PlayOneTime("grab");
             } else {
                 player.grabbingItem = false;
@@ -26,6 +30,9 @@ public class LiftAbility : MonoBehaviour
                 player.playerSpeed = 20f;
                 player.box.transform.SetParent(null, true);
                 player.box.GetComponent<Rigidbody>().isKinematic = false;
+
+                player.box.GetComponent<Collider>().isTrigger = false;
+
             }
     
         }
