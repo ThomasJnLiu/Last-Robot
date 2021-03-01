3d1f69574410cf88a08641ca51c1fe74faf6b286
Thomasjnliu <thomasjnliu@gmail.com>
2021-03-01 17:45:06 -0500

    Add 2 tutorial levels

---
+++ Assets/Scripts/TutorialLevel/RedButtonDoor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedButtonDoor : MonoBehaviour
+{
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter (Collision collision)
+    {
+        door.SetActive(false);
+    }
+    private void OnCollisionStay (Collision collision)
+    {
+        door.SetActive(false);
+    }
+
+    private void OnCollisionExit (Collision collision){
+        door.SetActive(true);
+    }
+}
