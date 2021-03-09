d8a8ee7fa2abb30f5a0a031d8ec0ecc8b3689276
k-rtik <kartikrajkanna@gmail.com>
2021-03-09 03:26:00 -0500

    initial

---
+++ Assets/Scripts/Controls/ControlsContext.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControlsContext : MonoBehaviour
+{
+    // Status
+    public bool isDisabled {get; private set; }
+    public Context context { get; private set; }
+
+    // Context Out
+    private Text text;
+
+    /**
+     * Three different contexts
+     **/
+    public enum Context
+    {
+        Box,
+        Broken_Lever,
+        Lever,
+        None
+    }
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+        text.enabled = false;
+
+        // Initial
+        isDisabled = false;
+        context = Context.None;
+    }
+
+    /**
+     * Get context based on the tag that the player is near
+     **/
+    Context GetContextFromTag(string tag)
+    {
+        switch (tag)
+        {
+            case "Box":
+                return Context.Box;
+
+            case "Broken_Lever":
+                return Context.Broken_Lever;
+
+            case "Lever":
+                return Context.Lever;
+            
+            default:
+                return Context.None;
+        }
+    }
+
+    /**
+     * Get prompt text based on context
+     **/
+    string GetContextText(Context context)
+    {
+        switch (context)
+        {
+            case Context.Box:
+                return "Pick up box";
+
+            case Context.Broken_Lever:
+                return "Disassemble arm to fix lever";
+
+            case Context.Lever:
+                return "Pull lever";
+            // None
+            default:
+                return "";
+        }
+    }
+
+     
+
+    /**
+     * Set control based on context and extrinsic variables.
+     * Returns true if context state is enabled; else false
+     **/
+    public void AddContextFromTag(string tag)
+    {
+        text.enabled = true;
+        text.text = GetContextText(GetContextFromTag(tag));
+    }
+
+    /**
+     *  Disable controls context when the object leaves the player's context
+     **/
+    public void DisableControlsContext()
+    {
+        text.enabled = false;
+    }
+
+    /**
+     * Set whether the controls are allowed
+     **/
+    public void SetControlsContextAbility(bool isEnabled)
+    {
+        isDisabled = isEnabled;
+    }
+}
--- Assets/Scripts/PlayerInteractRaycast.cs
+++ Assets/Scripts/PlayerInteractRaycast.cs
@@ -7,28 +7,37 @@ public class PlayerInteractRaycast : MonoBehaviour
     public PlayerController player;
     public LayerMask interactableLayer;
     public bool hittingInteractable = false;
+
+    public ControlsContext controlsContext;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-            RaycastHit hit;
+        RaycastHit hit;
 
-            Vector3 forward = transform.TransformDirection(Vector3.forward).normalized;
+        Vector3 forward = transform.TransformDirection(Vector3.forward).normalized;
 
-            if(Physics.SphereCast(new Vector3(transform.position.x, transform.position.y-3, transform.position.z),2f, forward, out hit, 3f, interactableLayer)){
-                player.GetGrabTaget(hit.transform.gameObject);
-                player.canGrab = true;
-            }else{
-                player.canGrab = false;
-            }
+        if (Physics.SphereCast(new Vector3(transform.position.x, transform.position.y - 3, transform.position.z), 2f, forward, out hit, 3f, interactableLayer))
+        {
+            player.GetGrabTaget(hit.transform.gameObject);
+            controlsContext.AddContextFromTag(hit.transform.gameObject.tag);
+            player.canGrab = true;
+        }
+        else
+        {
+            controlsContext.DisableControlsContext();
+            player.canGrab = false;
+        }
     }
 
-    void OnDrawGizmos(){
+    void OnDrawGizmos()
+    {
         float maxDistance = 5f;
         RaycastHit hit;
 
@@ -38,10 +47,9 @@ public class PlayerInteractRaycast : MonoBehaviour
         Gizmos.matrix = this.transform.localToWorldMatrix;
 
         // Representing the spherecast through gizmos was a big
-        Gizmos.color = new Color(1,0,0,0.5f);
-        Gizmos.DrawRay(new Vector3(0,-2,0), new Vector3(0,0,1f));
-        Gizmos.DrawWireSphere(new Vector3(0,-2,4), 3f);
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawRay(new Vector3(0, -2, 0), new Vector3(0, 0, 1f));
+        Gizmos.DrawWireSphere(new Vector3(0, -2, 4), 3f);
 
     }
-
 }
